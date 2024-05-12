6290a434498503b5afca7f92a9312af854f586a2
kila1 <kila14081996@gmail.com>
2024-05-12 20:02:29 +0300

    Дз с Исключениями

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import Exceptions.WrongLoginException;
+import Exceptions.WrongPasswordException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            TestExceptions.enteringPersonalData("Gorec_96",
+                    "java_234",
+                    "java_234");
+        } catch (WrongLoginException e){
+            System.out.println("В поле <логин> возможны только латиница, цифры и символ _");
+        } catch (WrongPasswordException e){
+            System.out.println("Проверьте правильность написания поля <пароль>," +
+                    " возможны только латиница, цифры и символ _. Пароли должны совпадать.");
+        }
+
     }
 }
